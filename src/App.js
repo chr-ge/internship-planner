c9f8d4173bfdae4f742d97e904a2f94d1bd8ce56
chr-ge <ace26@protonmail.com>
2020-05-10 00:53:49 -0400

    created Internship component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 //Components
 import Navbar from './components/Navbar';
@@ -10,20 +12,42 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#B0F4E6',
+      main: '#12D3CF',
+      dark: '#0D0221',
+      contrastText: '#FCF9EC'
+    },
+    secondary: {
+      light: '#B0F4E6',
+      main: '#67EACA',
+      dark: '#12D3CF',
+      contrastText: '#FCF9EC'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
 const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route path="/" component={home} />
-            <Route path="/login" component={login} />
-            <Route path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route path="/" component={home} />
+              <Route path="/login" component={login} />
+              <Route path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Internship from '../components/Internship';
+
+//material-ui
+import Grid from '@material-ui/core/Grid';
 
 export class home extends Component {
+    state = {
+        internships: null
+    }
+    
+    componentDidMount(){
+        axios.get('/internships')
+            .then(result => {
+                console.log(result.data);
+                this.setState({
+                    internships: result.data
+                })
+            })
+            .catch(error => console.log(error));
+    }
+
     render() {
+        let recentInternshipsMarkup = this.state.internships 
+            ? (this.state.internships.map((internship) => <Internship internship={internship} />)) 
+            : <p>Loading...</p>;
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <Grid container spacing={16}>
+                <Grid item sm={8} xs={12}>
+                    {recentInternshipsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile..</p>
+                </Grid>
+            </Grid>
         )
     }
 }
