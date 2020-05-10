c05b54dcb06fc696db3e170652d65001a67bad9d
chr-ge <ace26@protonmail.com>
2020-05-09 22:09:49 -0400

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+//Components
+import Navbar from './components/Navbar';
+
+//Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route path="/" component={home} />
+            <Route path="/login" component={login} />
+            <Route path="/signup" component={signup} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
